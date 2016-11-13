4f909241f46ad88c8f167867417597170b20f19b
Mark Montvai <mark.montvai@gmail.com>
2016-11-13 14:12:55 +0100

    Spaces here and there

--- src/app/app.declarations.ts
+++ src/app/app.declarations.ts
@@ -1,29 +1,23 @@
-import {NotFound404Component} from './components/notfound404/notfound404.component';
-import {MapsComponent} from "./containers/maps.component";
-import {WeaponUsageComponent} from "./components/weapon-usage.component";
-import {WeaponGuardianComponent} from "./components/weapon-guardian.component";
-import {QuickStats} from "./components/quick-stats.component";
-import {PlayerComponent} from "./containers/player.component";
-import {Emblem} from "./components/emblem.component";
-import {ActivityChartComponent} from "./components/activities-chart.component";
-import {ReportComponent} from "./containers/report.component";
-import {MenuComponent} from "./components/menu/menu.component";
-import {WinPercentagePipe} from "./pipes/win-percentage.pipe";
-import {SumFlawlessPipe} from "./pipes/sum-flawless.pipe";
-import {StreakPipe, StreakIconPipe} from "./pipes/streak.pipe";
-import {FooterComponent} from "./components/footer.component";
-import {EquippedGearComponent} from "./components/equipped/equipped-gear.component";
-import {ActivityChartPipe} from "./pipes/activity-chart.pipe";
-import {SubclassComponent} from "./components/equipped/subclass.component";
-import {ClassStatsComponent} from "./components/equipped/class-stats.component";
-import {
-  FilterWeapons,
-  FilterSubclass,
-  FilterArmor,
-  FilterClassStats,
-  FilterClassArmor
-} from "./pipes/filter-items.pipe";
-import {EquippedTabComponent} from "./components/equipped/equipped-tab.component";
+import { NotFound404Component } from './components/notfound404/notfound404.component';
+import { MapsComponent } from "./containers/maps.component";
+import { WeaponUsageComponent } from "./components/weapon-usage.component";
+import { WeaponGuardianComponent } from "./components/weapon-guardian.component";
+import { QuickStats } from "./components/quick-stats.component";
+import { PlayerComponent } from "./containers/player.component";
+import { Emblem } from "./components/emblem.component";
+import { ActivityChartComponent } from "./components/activities-chart.component";
+import { ReportComponent } from "./containers/report.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { WinPercentagePipe } from "./pipes/win-percentage.pipe";
+import { SumFlawlessPipe } from "./pipes/sum-flawless.pipe";
+import { StreakPipe, StreakIconPipe } from "./pipes/streak.pipe";
+import { FooterComponent } from "./components/footer.component";
+import { EquippedGearComponent } from "./components/equipped/equipped-gear.component";
+import { ActivityChartPipe } from "./pipes/activity-chart.pipe";
+import { SubclassComponent } from "./components/equipped/subclass.component";
+import { ClassStatsComponent } from "./components/equipped/class-stats.component";
+import { FilterWeapons, FilterSubclass, FilterArmor, FilterClassStats, FilterClassArmor } from "./pipes/filter-items.pipe";
+import { EquippedTabComponent } from "./components/equipped/equipped-tab.component";
 
 export const APP_DECLARATIONS = [
   MenuComponent,
